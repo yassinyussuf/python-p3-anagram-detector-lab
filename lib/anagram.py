5793bcce3faeb6942d35893a6564840c379aac0f
yassinyussuf <yassinyussuf414@gmail.com>
2024-05-21 15:43:09 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        matches = []
+        for w in word_list:
+            if sorted(w) == sorted(self.word):
+                matches.append(w)
+        return matches
